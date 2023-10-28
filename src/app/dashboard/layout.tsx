04d8bf291011d439fd01bba2f326b7fd8f1625db
Zsófia Maruzs <zsofia@splendex.io>
2023-10-28 22:58:27 +0200

    fix: add temporary fix to the window undefined issue

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,14 +1,20 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Sidebar from '../components/Sidebar';
 import ShowSidebarButton from '../components/ShowSidebarButton';
 
 const DashboardLayout = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
-  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-  const [theme, setTheme] = useState(defaultDark ? 'dark' : 'light');
+  const [theme, setTheme] = useState('light');
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+      setTheme(defaultDark ? 'dark' : 'light');
+    }
+  }, []);
 
   return (
     <>
