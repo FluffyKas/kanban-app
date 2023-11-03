fa88d16801cf161fa942343191f4e2c3a1297b66
Zsófia Maruzs <zsofia@splendex.io>
2023-11-03 21:15:03 +0100

    feat: add header dropdown

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,22 +5,29 @@ import KanbanLogoDark from '../assets/images/logo-dark.svg';
 import MoreOptionsIcon from '../assets/images/icon-vertical-ellipsis.svg';
 import Button from './Button';
 import { useTheme } from '../context/ThemeContext';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu';
 
 const Header = () => {
   const { theme } = useTheme();
 
   return (
-    <header className={`flex justify-between px-8 py-5 ${theme === 'dark' ? 'bg-kanban_grey_800' : 'bg-white'}`}>
+    <header className={`flex justify-between px-8 py-5 ${theme === 'dark' ? 'bg-kanban_grey_800 text-white' : 'bg-white'}`}>
       <div className="flex items-center">
-        <Image className="mr-36" src={theme === 'dark' ? KanbanLogoDark : KanbanLogoLight} alt="Kanban" />
+        <Image className="mr-[8.75rem]" src={theme === 'dark' ? KanbanLogoDark : KanbanLogoLight} alt="Kanban" />
         {/* TODO: Hardcoded value, need to change later */}
-        <h1 className="board-title">Platform Launch</h1>
+        <h1 className="board-title | text-2xl font-bold">Platform Launch</h1>
       </div>
-      <div className="flex gap-6">
+      <div className="flex gap-6 relative">
         <Button text="+ Add New Task" type="primary" size="large" clickHandler={() => {}} />
-        <button aria-label="More options">
-          <Image src={MoreOptionsIcon} alt="" aria-hidden="true" />
-        </button>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Image src={MoreOptionsIcon} alt="" aria-hidden="true" />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="bg-white p-4 rounded-lg right-4 absolute w-48">
+            <DropdownMenuItem className="cursor-pointer">Edit board</DropdownMenuItem>
+            <DropdownMenuItem className="cursor-pointer text-kanban_red">Delete board</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </header>
   );
